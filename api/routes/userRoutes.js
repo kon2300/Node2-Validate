9996fbc29f5d0b0c6ac08c8d359b515b35fa9fd8
Y8kon <Y8kon@konMacBook-Air.local>
2021-06-04 12:22:30 +0900

    Create a file dedicated to validation

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,34 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const usersController = require('../controllers/usersController');
-const validate = () => {
-  return [
-    check('password').isLength({ min:7 }).withMessage('パスワードは7文字以上にしてください。')
-    .custom((value, {req} ) => {
-      if (req.body.password !== req.body.passwordConfirmation) {
-        throw new Error('パスワード（確認）と一致しません。');
-      }
-      return true;
-    })
-  ]
-};
-
+const { validationResult } = require('express-validator');
+const v = require('../utils/validate');
 
 router.get('/signIn', usersController.signInView);
 router.get('/signUp', usersController.signUpView); 
-router.post('/create', validate(), (req, res, next) => {
+router.post('/create', v.validate(), (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
     req.skip = false;
-    console.log(req.skip);
     next();
   } else {
     let messages = errors.array().map(e => e.msg);
     req.skip = true;
     req.flash('errors', messages.join(''));
     res.locals.redirect = 'users/signUp';
-    console.log(req.skip);
     next();
   }
 }, usersController.checkCreate, usersController.create, usersController.redirectView);
